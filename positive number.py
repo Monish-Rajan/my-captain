91d90328867fbdfab69155b27b85235b10095f43
Monish-Rajan <117763057+Monish-Rajan@users.noreply.github.com>
2022-11-26 02:45:39 -0800

    Add files via upload

---
+++ positive number.py
@@ -0,0 +1,17 @@
+a = []
+x = []
+n = int(input("enter the no of inputs :"))
+b = n
+n = 0
+while b > n :
+    c =int(input("enter the numbers :"))
+    a.append(c)
+    n = n+1
+print(a)    
+
+for i in a:
+    if i > 0 :
+        x.append(i)
+print(x)       
+ 
+    
