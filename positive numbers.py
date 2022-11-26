3d825f5967c6ca9bf5bab6bd3ad6cbcbddc218f4
Monish-Rajan <117763057+Monish-Rajan@users.noreply.github.com>
2022-11-26 02:46:02 -0800

    Add files via upload

---
+++ positive numbers.py
@@ -0,0 +1,15 @@
+a = []
+n = int(input("enter the no of terms :"))
+b = n
+n = 0
+while b > n :
+    c =int(input("enter the numbers :"))
+    a.append(c)
+    n = n+1
+print(a)
+for i in a :
+    if i > 0 :
+        print(i)
+
+
+        
